b8c25f42d8ebd0313168722545ee7051492874a6
pchatard <chatard.pierre@gmail.com>
2020-12-02 15:12:29 +0100

    Update database config for dev

--- config/database.js
+++ config/database.js
@@ -1,17 +1,31 @@
-module.exports = ({ env }) => ({
+let configSettings = {
+  client: "postgres",
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: { rejectUnauthorized: false },
+};
+
+if (process.env.NODE_ENV) {
+  configSettings = {
+    client: "postgres",
+    host: process.env.DATABASE_HOST_DEV,
+    port: process.env.DATABASE_PORT_DEV,
+    database: process.env.DATABASE_NAME_DEV,
+    username: process.env.DATABASE_USERNAME_DEV,
+    password: process.env.DATABASE_PASSWORD_DEV,
+    ssl: false,
+  };
+}
+
+module.exports = () => ({
   defaultConnection: "default",
   connections: {
     default: {
       connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: process.env.DATABASE_HOST,
-        port: process.env.DATABASE_PORT,
-        database: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USERNAME,
-        password: process.env.DATABASE_PASSWORD,
-        ssl: { rejectUnauthorized: false },
-      },
+      settings: configSettings,
       options: {},
     },
   },
